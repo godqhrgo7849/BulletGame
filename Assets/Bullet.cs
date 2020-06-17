a656252fea620c40f7c4cc68dfb38b574d45ac77
godqhrgo7849 <63216333+godqhrgo7849@users.noreply.github.com>
2020-06-17 10:12:17 +0900

    6/17(김재홍)
    ㅇ

---
+++ Assets/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 8f;
+    private Rigidbody BulletRigidbody;
+    void Start()
+    {
+        BulletRigidbody = GetComponent<Rigidbody>();
+        BulletRigidbody.velocity = transform.forward * speed;
+        Destroy(gameObject, 3f);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            if (playerController != null)
+            {
+                playerController.Die();
+            }
+        }
+    }
+
+    
+}
+
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody PlayerRigidbody;
+    public float speed = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRigidbody = GetComponent<Rigidbody>();
+    }
+    
+
+// Update is called once per frame
+void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        PlayerRigidbody.velocity = newVelocity;
+    }
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
